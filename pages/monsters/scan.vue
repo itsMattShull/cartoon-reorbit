b23f0c119e9070c4af64156f37547660b5a0550a
Matt Shull <derekshull@gmail.com>
2025-12-25 20:46:19 -0600

    changes

--- pages/monsters/scan.vue
+++ pages/monsters/scan.vue
@@ -1,10 +1,11 @@
 <template>
   <Nav />
   <div class="mt-6 md:mt-12">&nbsp;</div>
+  <div class="mt-2 md:mt-6">&nbsp;</div>
   <div class="mx-auto max-w-3xl px-4 text-center text-sm text-slate-600 mt-6 md:mt-12">
     Scan barcodes or QR codes to discover monsters and items. QR codes are more reliable than barcodes.
   </div>
-  <BarcodeQrScanner class="mt-12" :show-view-monsters="true" />
+  <BarcodeQrScanner class="mt-4" :show-view-monsters="true" />
 </template>
 
 <script setup>
